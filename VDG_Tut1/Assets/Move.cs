d2dc756d78e7c6da3d8f8051d134df765a312ad1
WittyGamer277 <bwitt277@gmail.com>
2017-01-26 15:03:31 -0600

    Prototype Gun
    This is the test game ruben made during our last meeting.

---
+++ VDG_Tut1/Assets/Move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    [SerializeField]
+    Transform bullet;
+
+    [SerializeField]
+    Transform spawner;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 1 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -1 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(1 * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, spawner.position, spawner.rotation);
+        }
+    }
+}
---
+++ VDG_Tut1/Assets/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * Time.deltaTime * 10); // (0,1,0)
+
+        RaycastHit hit;
+        if(Physics.Raycast(new Ray(transform.position, Vector2.up), out hit, 1))
+        {
+            //destroy collision object using hit.transform.gameobject
+            Destroy(hit.transform.gameObject);
+        }
+	}
+}
