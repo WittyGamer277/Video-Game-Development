6a0e8653dbfc406956c654a8a7f0c0e37bfdfc64
RubenTipparach <santipab.stephane@gmail.com>
2017-02-07 20:37:43 -0600

    Zombie shooter from last year.

---
+++ FunTime/Assets/Gun.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	[SerializeField]
+	GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) && bullet != null)
+		{
+			var gb = GameObject.Instantiate(bullet, transform.position + transform.forward * 2, transform.rotation) as GameObject;
+
+			var rb = gb.GetComponent<Rigidbody>();
+
+			rb.AddForce(transform.forward * 1000);
+		}
+	}
+}
---
+++ FunTime/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	Text healthText;
+
+	int health = 100;
+
+	// Use this for initialization
+	void Start () {
+		healthText.text = health.ToString();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakeDamage(int amt)
+	{
+		health -= amt;
+		healthText.text = health.ToString();
+
+		if(health <= 0)
+		{
+			Application.LoadLevel("dead");
+			Destroy(this.gameObject);
+		}
+	}
+}
